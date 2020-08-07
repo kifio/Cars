eb696d0b3d79d5c9307b2be6af44f824086ae345
kifio <kg.of.morphine@gmail.com>
2020-08-07 09:08:52 +0200

    Initial Commit

---
+++ Cars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cars
+//
+//  Created by Ivan Murashov on 07.08.2020.
+//  Copyright © 2020 Ivan Murashov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
